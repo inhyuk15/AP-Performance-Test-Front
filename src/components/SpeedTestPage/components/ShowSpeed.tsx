3a113beb646281d5728337d11f7aedaca3f84cd4
InsunBaek <xecell515@gmail.com>
2023-05-28 16:53:30 +0900

    feat: 속도 측정 게이지바 추가, /src/SpeedTestPage/components/ShowSpeed.tsx +) 속도 게이지 측정함수들 mbpsToAmount, ocillate
    recoil 변수 수정, NetworkIndex 삭제, 이전의 SpeedtestData 타입을 이용하는 speedtestDataState 추가

--- src/components/SpeedTestPage/components/ShowSpeed.tsx
+++ src/components/SpeedTestPage/components/ShowSpeed.tsx
@@ -1,9 +1,20 @@
 import { Grid } from '@mui/material';
 import { useRecoilValue } from 'recoil';
-import { NetWorkIndexState } from '../../../module/Atom';
+import ReactSpeedometer from 'react-d3-speedometer';
+import { speedTestDataState } from '../../../module/Atom';
+
+const mbpsToAmount = (s: number): number => {
+  const ret = 1 - 1 / 1.3 ** Math.sqrt(s);
+  return ret;
+};
+
+const oscillate = (): number => {
+  const ret = 1 + 0.02 * Math.sin(Date.now() / 100);
+  return ret;
+};
 
 const ShowSpeed = () => {
-  const netWorkIndex = useRecoilValue(NetWorkIndexState);
+  const speedTestData = useRecoilValue(speedTestDataState);
   return (
     <div>
       <Grid container spacing={2}>
@@ -11,27 +22,56 @@ const ShowSpeed = () => {
           <strong>Avg Ping(ms):</strong>
         </Grid>
         <Grid item xs={6} sm={6} style={{ textAlign: 'left' }}>
-          {netWorkIndex.avgPing}
+          {speedTestData.pingStatus}
         </Grid>
         <Grid item xs={6} sm={6} style={{ textAlign: 'right' }}>
           <strong>Jitter(ms):</strong>
         </Grid>
         <Grid item xs={6} sm={6} style={{ textAlign: 'left' }}>
-          {netWorkIndex.jitter}
+          {speedTestData.jitterStatus}
         </Grid>
         <Grid item xs={6} sm={6} style={{ textAlign: 'right' }}>
           <strong>Upstream(kb/s):</strong>
         </Grid>
         <Grid item xs={6} sm={6} style={{ textAlign: 'left' }}>
-          {netWorkIndex.upstreamSpeed}
+          {speedTestData.ulStatus}
         </Grid>
         <Grid item xs={6} sm={6} style={{ textAlign: 'right' }}>
           <strong>Downstream(kb/s):</strong>
         </Grid>
         <Grid item xs={6} sm={6} style={{ textAlign: 'left' }}>
-          {netWorkIndex.downstreamSpeed}
+          {speedTestData.dlStatus}
         </Grid>
       </Grid>
+
+      {/* 속도 계기판 */}
+      <ReactSpeedometer
+        minValue={0}
+        maxValue={1}
+        value={mbpsToAmount(
+          speedTestData.dlStatus *
+            (speedTestData.testState === 1 ? oscillate() : 1)
+        )}
+        segments={20}
+        needleColor="black"
+        startColor="red"
+        endColor="green"
+        maxSegmentLabels={5}
+      />
+
+      <ReactSpeedometer
+        minValue={0}
+        maxValue={1}
+        value={mbpsToAmount(
+          speedTestData.ulStatus *
+            (speedTestData.testState === 1 ? oscillate() : 1)
+        )}
+        segments={20}
+        needleColor="black"
+        startColor="red"
+        endColor="green"
+        maxSegmentLabels={5}
+      />
     </div>
   );
 };
